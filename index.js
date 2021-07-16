984d3e3f72507b5d0e7b32b1459131af0e6d490f
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-07-16 17:41:34 -0400

    using docker pull?

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const child_process = require('child_process')
+
+// const pull = ({imageName}) => {
+// 	console.log(`Pulling docker image (${imageName})`);
+// 	child_process.execSync(
+// 		`docker pull ${imageName}`
+// 	);
+// };
 
 try {
 	// `docker-image` input defined in action metadata file
 	const dockerImage = core.getInput('docker-image');
 	console.log(`The docker repo is ${dockerImage}.`);
+
+	console.log(`Pulling docker image (${dockerImage})`);
+	child_process.execSync(
+		`docker pull ${dockerImage}`
+	);
 } catch (error) {
 	core.setFailed(error.message);
 }
